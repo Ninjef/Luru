3c0aa2df4179e99d881816d8dac7ee8316017016
Ninjef <jeff.arnold@welltok.com>
2016-10-24 10:34:16 -0600

    get language param to work better

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,6 +2,10 @@ class Language < ActiveRecord::Base
   validates :name, presence: true
   validates :code, presence: true
 
+  def url_parameter
+    "?lang=#{code}"
+  end
+
   def to_s
     name
   end
